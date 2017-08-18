549165027e261ae9e744a8c7cb8a8fbd645f2212
Tony Simoes <sr.tony@gmail.com>
2017-08-18 18:25:05 +0200

    Tarea 2 acabada:
    Modificar el controlador CallHistoryControler

--- PhoneApp/CallHistoryControler.cs
+++ PhoneApp/CallHistoryControler.cs
@@ -1,13 +1,53 @@
 ﻿using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace PhoneApp
 {
     public partial class CallHistoryControler : UITableViewController
     {
-        public CallHistoryControler (IntPtr handle) : base (handle)
+
+        public List<string> PhoneNumbers { get; set; }
+
+        protected NSString CallHistoryCellID = new NSString("CallHistoryCell");
+
+
+        public CallHistoryControler(IntPtr handle) : base(handle)
         {
+            TableView.RegisterClassForCellReuse(typeof(UITableViewCell),CallHistoryCellID);
+            TableView.Source = new CallHistoryDataSource(this);
+        }
+        
+
+        class CallHistoryDataSource : UITableViewSource
+        {
+
+            CallHistoryControler Controler;
+
+            public CallHistoryDataSource(CallHistoryControler controler)
+            {
+                // Almacenar la instancia del UITableViewController
+                this.Controler = controler;
+
+            }
+
+            //
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+            {
+                // Devuelve un UITableViewCell reutilizable creado con el identificador
+                // Controler.CallHistoryCellID
+                var cell = tableView.DequeueReusableCell(Controler.CallHistoryCellID);
+
+                cell.TextLabel.Text = Controler.PhoneNumbers[indexPath.Row];
+
+                return cell;
+            }
+
+            public override nint RowsInSection(UITableView tableview, nint section)
+            {
+                return Controler.PhoneNumbers.Count;
+            }
         }
     }
 }
